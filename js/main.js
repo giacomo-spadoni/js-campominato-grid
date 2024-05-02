c2860b66e467fbef98e6878cf17442127a6aeb7a
giacomo-spadoni <giaky.spado@gmail.com>
2024-05-02 10:21:14 +0200

    3rd Milestone complited

--- js/main.js
+++ js/main.js
@@ -3,5 +3,14 @@ const griglia = document.getElementById('grid')
 for (let i = 0; i < 9; i++) {
     let quadrato = document.createElement('div')
     quadrato.classList.add('square')
+    quadrato.addEventListener('click', function () {
+        quadrato.classList.toggle('evidenziato')
+        if (quadrato.classList.contains('evidenziato')) {
+            quadrato.textContent = i
+        } else {
+            quadrato.textContent = ''
+        }
+    })
     griglia.append(quadrato)
 }
+
