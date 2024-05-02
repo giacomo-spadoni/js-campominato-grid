84e428ed17fc8033ef9ed7a1e9a9f25bf73249e4
giacomo-spadoni <giaky.spado@gmail.com>
2024-05-02 11:06:01 +0200

    improved code cleanliness

--- js/main.js
+++ js/main.js
@@ -1,8 +1,46 @@
 const griglia = document.getElementById('grid')
+const facile = document.getElementById('easy')
+const medio = document.getElementById('standard')
+const difficile = document.getElementById('hard')
+const fine = document.getElementById('end')
 
-for (let i = 0; i < 9; i++) {
-    addQuadrato(griglia, i)
-}
+fine.addEventListener('click', function () {
+    show()
+
+    griglia.classList = ''
+    griglia.innerHTML = ''
+})
+
+facile.addEventListener('click', function () {
+    hidden()
+
+    griglia.classList.add('easy')
+
+    for (let i = 0; i < 100; i++) {
+        addQuadrato(griglia, i)
+    }
+})
+
+medio.addEventListener('click', function () {
+    hidden()
+
+    griglia.classList.add('standard')
+
+    for (let i = 0; i < 81; i++) {
+        addQuadrato(griglia, i)
+    }
+})
+
+difficile.addEventListener('click', function () {
+    hidden()
+
+    griglia.classList.add('hard')
+
+    for (let i = 0; i < 49; i++) {
+        addQuadrato(griglia, i)
+    }
+})
+
 
 function addQuadrato(griglia, i) {
     let quadrato = document.createElement('div')
@@ -16,4 +54,18 @@ function addQuadrato(griglia, i) {
         }
     })
     griglia.append(quadrato)
+}
+
+function show() {
+    facile.classList.remove('hidden')
+    medio.classList.remove('hidden')
+    difficile.classList.remove('hidden')
+    fine.classList.add('hidden')
+}
+
+function hidden() {
+    facile.classList.add('hidden')
+    medio.classList.add('hidden')
+    difficile.classList.add('hidden')
+    fine.classList.remove('hidden')
 }
